90c7f3856bcfe4a65c908252e2453b9119663fc3
RafaelPassosS <128323662+RafaelPassosS@users.noreply.github.com>
2023-06-15 20:17:39 -0300

    Update ProvaKamilla2.java

--- ProvaKamilla2.java
+++ ProvaKamilla2.java
@@ -5,31 +5,34 @@ import java.util.ArrayList;
 public class ProvaKamilla2 {
     static ArrayList<Cliente> Clientes = new ArrayList<>();
     static ArrayList<Vendedor> Vendedores = new ArrayList<>();
-
+    static ArrayList<Produto> Produtos = new ArrayList<>();
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         String opc = "10";
         while(opc != "0"){
+            Scanner entrada = new Scanner(System.in);
             System.out.print("1- Cadastrar Clientes");
             System.out.print("2 - Cadastrar Vendedor");
             System.out.print("3 - Cadastrar Produto");
             System.out.print("4 - Vender Produto");
             System.out.print("5 - Relatorio de Vendas");
+            System.out.print("Selecione sua opção: ");
             
-            if(opc.equals('1')){
-                Scanner entrada = new Scanner(System.in);
+            opc = entrada.next();
+            
+            if(opc.equals("1")){
                 Cliente cliente = new Cliente();
-                System.out.println("insira o Codigo do cliente: ");
+                System.out.println("insira o Codigo do cliente: \n");
                 String codigo = entrada.nextLine();
-                System.out.println("Insira o Nome do Cliente: ");
+                System.out.println("Insira o Nome do Cliente: \n");
                 String nome = entrada.nextLine();
-                System.out.println("Insira o endereco do Cliente; ");
+                System.out.println("Insira o endereco do Cliente; \n");
                 String endereco =  entrada.nextLine();
-                System.out.println("Insira o Telefone do cliente: ");
+                System.out.println("Insira o Telefone do cliente: \n");
                 String telefone = entrada.nextLine();
-                System.out.println("Insira o CPF do Cliente: ");
+                System.out.println("Insira o CPF do Cliente: \n");
                 String cpf = entrada.nextLine();
                 
                 cliente.cadastrarCliente(codigo, nome, endereco, telefone, cpf);
