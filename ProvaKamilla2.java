1c525551cc8fb5b0f5379a652fe3e674b5af438b
RafaelPassosS <128323662+RafaelPassosS@users.noreply.github.com>
2023-06-15 20:07:07 -0300

    Update ProvaKamilla2.java

--- ProvaKamilla2.java
+++ ProvaKamilla2.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class ProvaKamilla2 {
-    static ArrayList<Cliente> clientes = new ArrayList<>();
-    static ArrayList<Vendedor> vendedores = new ArrayList<>();
+    static ArrayList<Cliente> Clientes = new ArrayList<>();
+    static ArrayList<Vendedor> Vendedores = new ArrayList<>();
 
     /**
      * @param args the command line arguments
@@ -19,8 +19,37 @@ public class ProvaKamilla2 {
             System.out.print("5 - Relatorio de Vendas");
             
             if(opc.equals('1')){
+                Scanner entrada = new Scanner(System.in);
                 Cliente cliente = new Cliente();
-                System.out.println("Insira o nome do cliente; ")
+                System.out.println("insira o Codigo do cliente: ");
+                String codigo = entrada.nextLine();
+                System.out.println("Insira o Nome do Cliente: ");
+                String nome = entrada.nextLine();
+                System.out.println("Insira o endereco do Cliente; ");
+                String endereco =  entrada.nextLine();
+                System.out.println("Insira o Telefone do cliente: ");
+                String telefone = entrada.nextLine();
+                System.out.println("Insira o CPF do Cliente: ");
+                String cpf = entrada.nextLine();
+                
+                cliente.cadastrarCliente(codigo, nome, endereco, telefone, cpf);
+                Clientes.add(cliente);
+            }else{
+                if(opc.equals("2")){
+                    
+                }else{
+                    if(opc.equals("3")){
+                        
+                    }else{
+                        if(opc.equals("4")){
+                            
+                        }else{
+                            if (opc.equals("5")){
+                                
+                            }
+                        }
+                    }
+                }
             }
         }
         
